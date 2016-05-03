43814af82e38784d73c763681c7dcd2dc6ae1a39
Peter Whang <Peter Whang>
2016-05-03 13:37:56 -0700

    Change make_move

--- script.js
+++ script.js
@@ -14,18 +14,19 @@ function make_move() {
     //main function executed on click. add image to block, call check_win, call check_draw, increase play_count
     if(play_count == 1 || play_count%2 == 1){
        // var x = 'x';
-        var x = $('<div>').addClass('img-responsive').html('<img src="images/x.png">');
-        $(x).appendTo('<div>');
+        $('<img>').hasClass('.x').show();
+      //  var x = $('<div>').addClass('img-responsive').html('<img src="images/x.png">');
+      //  $(x).appendTo('<div>');
         play_count++;
         check_win();
         check_draw();
     }
     else{
        // var o = 'o';
-        var o = $('<div>').addClass('img-responsive').html('<img src="images/o.png">'); //addClass
-        $(o).appendTo('<div>');
+        $('<img>').hasClass('.o').show();
+       // var o = $('<div>').addClass('img-responsive').html('<img src="images/o.png">'); //addClass
+      //  $(o).appendTo('<div>');
         play_count++;
-       
         check_win();
         check_draw();
     }
@@ -52,7 +53,7 @@ function reset() {
         cell_count = 3;
         player1_wins = 0;
         player2_wins = 0;
-        $('.').removeClass(); // remove or toggle class??
+        $('.x, .o').(); // remove or toggle class??
 
     }
 
@@ -74,6 +75,7 @@ function display() {
 }
 
 $(document).ready(function(){
+    $('.image-responsive').hide();
     $('').click(function(){
        make_move();
     });
