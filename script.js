94fcc641c5428b9d60d2a52223e59847703749a8
Erika O'Neal <e.oneal89@gmail.com>
2016-05-03 13:22:05 -0700

    finished check_win

--- script.js
+++ script.js
@@ -160,6 +160,153 @@ function check_win(object) {
         }
 
     }
+    
+   /*************** PLAYER 2 CHECK ******************/
+    if (object.value == 'player2') {
+        var temp_check_array = [];
+        temp_check_array.push(object);
+
+        //check column win
+        for (i = 0; i < player2_array.length; i++) { //fill check array with all x's in that column
+            var current_object = player2_array[i];
+            if (current_object.column == object.column) {
+                temp_check_array.push(current_object);
+            }
+        }
+
+        if (temp_check_array >= cell_win_count) {       //checking if the x's are consecutive in that column
+
+            temp_check_array.sort(function compare(a, b) { //sorting array from least number to greatest number of column
+                if (a.column > b.column) {
+                    return 1;
+                }
+                else if (a.column < b.column) {
+                    return -1;
+                }
+                else {
+                    return 0;
+                }
+            });
+
+            var first_check_object = temp_check_array[0];
+            var current_check_object = temp_check_array[0];
+
+            for (i = 1; i < temp_check_array.length; i++) {
+
+                for (j = 1; j <= cell_win_count; j++) {
+                    if (temp_check_array[i].column == first_check_object.column + j) {
+                        current_check_object = temp_check_array[i];
+
+                        if (temp_check_array.indexOf(current_check_object) == temp_check_array.indexOf(first_check_object) + cell_win_count) {
+                            display('Player 1 Wins!');
+                            win = true;
+                        }
+                    }
+                    else {
+                        first_check_object = temp_check_array[i];
+                        break;
+                    }
+                }
+
+            }
+        }
+
+        //check row win
+        if (!win) {
+
+            var temp_check_array = [];
+            temp_check_array.push(object);
+
+            for (i = 0; i < player2_array.length; i++) { //fill check array with all x's in that column
+                var current_object = player2_array[i];
+                if (current_object.row == object.row) {
+                    temp_check_array.push(current_object);
+                }
+            }
+
+            if (temp_check_array >= cell_win_count) {       //checking if the x's are consecutive in that column
+
+                temp_check_array.sort(function compare(a, b) { //sorting array from least number to greatest number of column
+                    if (a.column > b.row) {
+                        return 1;
+                    }
+                    else if (a.row < b.row) {
+                        return -1;
+                    }
+                    else {
+                        return 0;
+                    }
+                });
+
+                var first_check_object = temp_check_array[0];
+                var current_check_object = temp_check_array[0];
+
+                for (i = 1; i < temp_check_array.length; i++) {
+
+                    for (j = 1; j <= cell_win_count; j++) {
+                        if (temp_check_array[i].row == first_check_object.row + j) {
+                            current_check_object = temp_check_array[i];
+
+                            if (temp_check_array.indexOf(current_check_object) == temp_check_array.indexOf(first_check_object) + cell_win_count) {
+                                display('Player 1 Wins!');
+                            }
+                        }
+                        else {
+                            first_check_object = temp_check_array[i];
+                            break;
+                        }
+                    }
+
+                }
+            }
+        }
+
+        //check diagonals
+        if (!win) {
+            temp_check_array = player2_array.splice();
+
+            temp_check_array.sort(function compare(a, b) { //sorting player 2 array from least number to greatest number of column
+                if (a.column > b.column) {
+                    return 1;
+                }
+                else if (a.column < b.column) {
+                    return -1;
+                }
+                else {
+                    return 0;
+                }
+            });
+
+            var first_check_object = temp_check_array[0];
+            var current_check_object = temp_check_array[0];
+
+            for (i = 1; i < temp_check_array.length; i++) {
+
+                for (j = 1; j <= cell_win_count; j++) {
+                    if (temp_check_array[i].row == first_check_object.row + j && temp_check_array[i].column == first_check_object.column + j) {
+                        current_check_object = temp_check_array[i];
+
+                        if (temp_check_array.indexOf(current_check_object) == temp_check_array.indexOf(first_check_object) + cell_win_count) {
+                            display('Player 1 Wins!');
+                        }
+                    }
+                    else if (temp_check_array[i].row == first_check_object.row - j && temp_check_array[i].column == first_check_object.column - j) {
+                        current_check_object = temp_check_array[i];
+
+                        if (temp_check_array.indexOf(current_check_object) == temp_check_array.indexOf(first_check_object) + cell_win_count) {
+                            display('Player 1 Wins!');
+                        }
+                    }
+                    else {
+                        first_check_object = temp_check_array[i];
+                        break;
+                    }
+                }
+
+            }
+        }
+
+    }
 }
 
 function check_draw() {
