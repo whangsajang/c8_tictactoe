22ed37abbb520cb391981edc5593e74c8d710ceb
Erika O'Neal <e.oneal89@gmail.com>
2016-05-03 10:36:08 -0700

    function skeleton

---
+++ script.js
@@ -0,0 +1,32 @@
+/**
+ * Created by erikaoneal on 5/3/16.
+ */
+var player1_array = [];
+var player2_array = [];
+var play_count = 1;
+var cell_count = 3;
+var cell_win_count = 3;
+
+function make_move() {
+    //main function executed on click. add image to block, call check_win, call check_draw, increase play_count
+}
+
+function check_win(object) {
+    //loop through array to compare wins
+}
+
+function check_draw() {
+    //see if gameboard is full and no win is present
+}
+
+function game_board() {
+    //dynamically creates board according to cell_count
+}
+
+function reset() {
+    //clear all the objects in arrays, clear gameboard
+}
+
+function display() {
+    //shows stats, and highlights which player's turn it is
+}
